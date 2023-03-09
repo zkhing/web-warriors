e014f3122a9036561f0a988ed2e993cfd2bfb13b
sasarkarimi <sahar.karimi90@outlook.com>
2023-03-09 11:40:21 +0000

    added loginpage

---
+++ .github/components/LoginPage.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here
+  }
+  return (
+    <div>
+      <img src="our-image-url.jpg" alt="our Image" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="Email">Email:</label>
+        <input
+          type="Email"
+          id="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="Password"
+          id="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="Submit">Sign In</button>
+      </form>
+      <a href="#">Forgot Password?</a>
+    </div>
+  );
+}
+export default LoginPage;
