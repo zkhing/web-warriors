a036f4768cb2f95134def760f2f7d112ec374dd4
sasarkarimi <sahar.karimi90@outlook.com>
2023-03-09 17:47:22 +0000

    add available  time sevice

--- server/api.js
+++ server/api.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-
+import db from "./db"
 import logger from "./utils/logger";
 
 const router = Router();
@@ -7,6 +7,16 @@ const router = Router();
 router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
+});
+
+router.post("/addAvailabeTime", (req, res) => {
+	logger.debug("Add Availabe Time");
+	db.query("insert into availabeTableName (studentId,date,from,to) values($1,$2,$3,$4) returning *",
+		[req.body.studentId, req.body.date, req.body.from, req.body.to],
+		(error, result) => {
+			res.json({ message: "Availabe Time Added " });
+		}
+	)
 });
 
 export default router;
