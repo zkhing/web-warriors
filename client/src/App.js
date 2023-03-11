a607d97aab05091ee005a283af9f7741636f77a0
Zaw Khing <zawmyokhing@gmail.com>
2023-03-11 17:13:34 +0000

    Install bootstrap library

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-// import LoginPage from "./components/LoginPage";
+import LoginPage from "./components/LoginPage";
 import About from "./pages/About";
 import Home from "./pages/Home";
 
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
+		<Route path="/login" element={<LoginPage />} />
 		<Route path="/about/this/site" element={<About />} />
 	</Routes>
 );
