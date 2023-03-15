8302a55f73a6b4f272e2cdb2a55ce79c13e0da00
migue <miguelanjo2@hotmail.com>
2023-03-15 11:29:18 +0000

    list availabilities

---
+++ client/src/components/MyAvailabilities.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+
+const MyAvailabilities = () => {
+ const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch(`api/localhost:3000/availabilities`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData.availabilities);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="table">
+      <tbody>
+       
+        <tr>
+          <th>Name</th>
+          <th>Availabilityid</th>
+          <th>Date</th>
+          <th>TimeFrom</th>
+          <th>TimeTo</th>
+       
+        </tr>
+        {data.map((data, index) => (
+          <tr key={index}>
+            <td>{data.username}</td>
+            <td>{data.availabilityid}</td>
+            <td>{data.date}</td>
+            <td>{data.time_from}</td>
+            <td>{data.time_to}</td>
+          </tr>
+        ))}
+      </tbody>
+    </div>
+  );
+  
+}
+
+export default MyAvailabilities
