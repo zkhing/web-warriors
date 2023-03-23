9131f7e406e53f1305ee69bd087cda1b379547f8
sasarkarimi <sahar.karimi90@outlook.com>
2023-03-23 20:13:10 +0000

    login button not workin without username

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -8,22 +8,27 @@ function LoginPage() {
 	const [username, setUsername] = useState("");
 
 	const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(username);
-    try {
-      const response = await fetch(`/api/users/${username}`);
-      const data = await response.json();
-      console.log(data);
-      if (data.length > 0) {
-        window.location.href = `InputAvailabilitiesPage?username=${username}`;
-      } else {
-        alert("The Username you entered does not match any student, try again!!");
-      }
-    } catch (error) {
-      console.error(error);
-      alert("An error occurred. Please try again later.");
-    }
-  };
+		event.preventDefault();
+		if (!username) {
+		  alert("Please enter a username!");
+		  return;
+		}
+		console.log(username);
+		try {
+		  const response = await fetch(`/api/users/${username}`);
+		  const data = await response.json();
+		  console.log(data);
+		  if (data.length > 0) {
+			window.location.href = `InputAvailabilitiesPage?username=${username}`;
+		  } else {
+			alert("The Username you entered does not match any student, try again!!");
+		  }
+		} catch (error) {
+		  console.error(error);
+		  alert("An error occurred. Please try again later.");
+		}
+	  };
+	
 
 	const handleInputChange = (event) => {
 		setUsername(event.target.value);
